679f5fa088bc21e29a1fe59fbf25c001ca19c39f
Julia Kieserman <jkieserman@wikimedia.org>
2022-11-18 15:16:10 -0500

    initial api call

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Button from '@mui/material/Button';
 
 export default function Home() {
   return (
